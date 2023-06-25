5ee6fa8acf1a63e68c3d159b32920f9f39aa4d16
AbdulrahmanAlq <itzd7o0@hotmail.com>
2023-06-25 23:18:01 +0300

    add oauth , use can register with google

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const findOrCreate = require('mongoose-findorcreate');
 // const encrypt = require('mongoose-encryption');
 // const bcrypt = require("bcrypt");
 // const saltround = 10;
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
 
 app.use(express.static("public"));
@@ -28,10 +30,12 @@ mongoose.connect("mongodb://0.0.0.0:27017/userDB");
 
 const userSchema = mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
 // userSchema.plugin(encrypt , {secret:process.env.SECRET , encryptedFields:["password"]});
@@ -40,12 +44,49 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.get("/", function (req, res) {
     res.render("home");
 })
+
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile"] })
+)
+
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
 
 app.get("/login", function (req, res) {
     res.render("login");
@@ -55,24 +96,24 @@ app.get("/register", function (req, res) {
     res.render("register");
 })
 
-app.get("/secrets", function(req , res ){
-    if(req.isAuthenticated()){
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
         res.render("secrets");
-    }else{
+    } else {
         res.redirect("/login");
     }
 })
 
 app.post("/register", function (req, res) {
 
-    User.register({username : req.body.username} , req.body.password , function(err , uesr){
-        if(err){
+    User.register({ username: req.body.username }, req.body.password, function (err, uesr) {
+        if (err) {
             console.log(err);
             res.redirect("/register");
-        }else{
-            passport.authenticate("local")(req , res , function(){
+        } else {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/secrets");
-            }) 
+            })
         }
     })
 })
@@ -83,26 +124,26 @@ app.post("/login", function (req, res) {
         username: req.body.username,
         password: req.body.password
     })
-    req.login(user, function(err){
-        if(err){
+    req.login(user, function (err) {
+        if (err) {
             console.log(err);
-        }else{
-            passport.authenticate("local")(req , res , function(){
+        } else {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/secrets");
             });
         }
     })
 })
 
-app.get("/logout" , function(req , res ){
-    req.logout(function(err){
-        if(err){
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) {
             console.log(err);
-        }else{
+        } else {
             res.redirect("/");
         }
     });
-   
+
 })
 
 app.listen("3000", function () {
